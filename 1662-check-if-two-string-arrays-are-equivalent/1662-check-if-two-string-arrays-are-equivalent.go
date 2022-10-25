7f720d23935b461467a047e9ba10e39b72667d63
Youssef Siam <youssef1yasser38@gmail.com>
2022-10-25 03:33:53 +0200

    Time: 0 ms (100.00%), Space: 2.7 MB (36.62%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.go
@@ -0,0 +1,11 @@
+func arrayStringsAreEqual(word1 []string, word2 []string) bool {
+	w1 := ""
+	w2 := ""
+	for _, v := range word1 {
+		w1 += v
+	}
+	for _, v := range word2 {
+		w2 += v
+	}
+	return w1 == w2
+}
